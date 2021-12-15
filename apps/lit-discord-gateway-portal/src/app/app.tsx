5eedc79d59c42ad342dfc74fbfc51977b42b2f9b
Jethro Fredericks- <jethro.fredericks@protonmail.com>
2021-12-15 21:47:56 +0200

    initial commit

---
+++ apps/lit-discord-gateway-portal/src/app/app.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+import NxWelcome from './nx-welcome';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <NxWelcome title="lit-discord-gateway-portal" />
+    </StyledApp>
+  );
+}
+
+export default App;
